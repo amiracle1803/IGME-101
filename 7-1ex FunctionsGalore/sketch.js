14f3de8a56c89844fdf50208d740f87f77bb79c8
Amir El (RIT Student) <ase8274@rit.edu>
2026-03-06 11:59:17 -0500

    asd

--- 7-1ex FunctionsGalore/sketch.js
+++ 7-1ex FunctionsGalore/sketch.js
@@ -1,63 +1,33 @@
 /**
  * Amir EL, 1/23/26
- * IGME-101: 4-2 demo math operators
+ * IGME-101: 7-1 demo math operators
  * Description and interaction instructions
  */
- 
 "use strict"; //catch some common coding errors
 
 /* Global variables */
-let x;
-let y;
-let speedX;
-let speedY;
+let imgAria;
+let imgJav;
 
+function preload(){
+    imgAria = loadImage("media/Harris_Aria.jpg");
+    imgJav = loadImage("media/jajks_sk.png");
+    console.log ("start loading",imgAria, imgJav); 
+}
 /**
  * setup :
  */
 function setup() {
-  createCanvas(400, 600);
-  speedX = 1; // behavior if -1?
-  speedY =1; // behavior if -1?
-  x = width/3;
-  y = height/3;
-  
-  fill("red"); //ball color
-
 }
 /**
  * draw :
  */
 
 function draw() {
-  background(100, 180, 255, 20);
+background("beige");
 
-  //draw the ball
-  ellipse(x, y, 20);
+  //fill navy
+fill("navy");
 
-  //If x is too big (at right edge of canvas), make speedX -1
-  if (x > width -10){
-    //go towards left
-    speedX = 1;
-  }
-  
-    //If x is too big (at right edge of canvas), make speedX -1
-  if (y > height -10){
-    //go towards left
-    speedY = 1;
-    console.log("nounce ball",y, speedY)
-  }
 
-    //If x is too big (at right edge of canvas), make speedX -1
-  if (y < -10){
-    //go towards left
-    speedY = 1;
-  }
-  
-  //move the ball
-
-  x = x + speedX;
-  y = y + speedY;
-
-  
 }
