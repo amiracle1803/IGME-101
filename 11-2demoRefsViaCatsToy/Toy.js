c57a66aa7c4497ec593c571f3068515950cfe03f
Amir El (RIT Student) <ase8274@rit.edu>
2026-04-01 12:44:21 -0400

    dlf

---
+++ 11-2demoRefsViaCatsToy/Toy.js
@@ -0,0 +1,34 @@
+/**
+ * Amir EL, 3/21/26
+ * IGME-101: Exercise 9-3:  lasses, object, and methods
+ * using functions to do stuff
+ * Description and interaction instructions
+ */
+
+"use strict"; //catch some common coding errors
+
+
+
+/* Global variables */
+
+/**
+ * setup :
+ */
+function setup() {
+createCanvas(600, 400);
+}
+
+class Cat {
+    constructor(){
+    this.pacHeight = 15;
+    this.propertyname = someValue;
+    this.color = "Yellow";
+    this.propertyname = someValue;
+    //If there is a toy,
+    //notify it has been dropprf befor null
+
+    this.TypeError.dropped();
+    this.toy = null;
+}
+}
+// by : amir el
---
+++ 11-2demoRefsViaCatsToy/sketch.js
@@ -0,0 +1,38 @@
+/**
+ * Amir EL, 3/21/26
+ * IGME-101: Exercise 9-3:  lasses, object, and methods
+ * using functions to do stuff
+ * Description and interaction instructions
+ */
+
+"use strict"; //catch some common coding errors
+
+
+/* Global variables */
+
+/**
+ * setup :
+ */
+function setup() {
+    createCanvas(600, 400);
+
+// for setup fuction
+//  Make a good size canvas --
+//  handle any mode, stroke, or text setting you will need.
+//  assign each global variable to the new instance of a class, setting appropriae
+//   argument to each class constructor .
+//   There should be  2 not - indentical  instance of each class 
+//   print the  each variable to the console so you can check that all properties were assign properlu.
+//   sample console result.
+
+
+}
+
+/**
+ * draw :
+ */
+function draw() {
+
+}
+
+// by : amir el
